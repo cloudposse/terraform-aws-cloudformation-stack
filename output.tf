3ec4ecccba5c3fcf62b97471855927254c2be11c
Igor Rodionov <goruha@gmail.com>
2019-11-11 18:18:38 +0600

    Added spotinst integration

--- output.tf
+++ output.tf
@@ -1,4 +1,15 @@
 output "name" {
   value       = "${join("", aws_cloudformation_stack.default.*.name)}"
-  description = "Name of the Stack Set"
+  description = "Name of the Stack"
 }
+
+output "id" {
+  value       = "${join("", aws_cloudformation_stack.default.*.id)}"
+  description = "ID of the Stack"
+}
+
+output "outputs" {
+  value       = "${var.enabled == "true" ? jsonencode(aws_cloudformation_stack.default.*.outputs) : jsonencode(map())}"
+  description = "Outputs of the Stack"
+}
+
