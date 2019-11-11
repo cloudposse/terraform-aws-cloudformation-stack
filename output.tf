f06dc68b9d038511b8e46bbc33231aa657bf2058
Igor Rodionov <goruha@gmail.com>
2019-11-11 17:11:31 +0600

    Added spotinst integration

--- output.tf
+++ output.tf
@@ -1,14 +1,9 @@
-output "administrator_role_arn" {
-  value       = "${module.executor_role.arn}"
-  description = "Amazon Resource Number (ARN) of the IAM Role in the administrator account"
-}
-
 output "executor_role_name" {
-  value       = "${var.executor_role_name}"
+  value       = "${module.executor_role.arn}"
   description = "Name of the IAM Role in all target accounts for Stack Set operations"
 }
 
 output "name" {
-  value       = "${join("", aws_cloudformation_stack_set.default.*.name)}"
+  value       = "${join("", aws_cloudformation_stack.default.*.name)}"
   description = "Name of the Stack Set"
 }
