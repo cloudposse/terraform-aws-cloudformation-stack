da4651597e92bdfaba7a6cda1375c3e9d0e57a81
Igor Rodionov <goruha@gmail.com>
2019-11-24 23:31:18 +0600

    Migrate to 0.12

--- main.tf
+++ main.tf
@@ -1,27 +1,31 @@
 # Define composite variables for resources
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.3"
-  enabled    = "${var.enabled}"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  enabled    = var.enabled
+  namespace  = var.namespace
+  environment = var.environment
+  stage      = var.stage
+  name       = var.name
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = var.tags
+  additional_tag_map = var.additional_tag_map
+  label_order = var.label_order
+  regex_replace_chars = var.regex_replace_chars
 }
 
 resource "aws_cloudformation_stack" "default" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+  count = var.enabled ? 1 : 0
 
-  name = "${module.label.id}"
-  tags = "${module.label.tags}"
+  name = module.label.id
+  tags = module.label.tags
 
-  template_url = "${var.template_url}"
-  parameters   = "${var.parameters}"
-  capabilities = ["${var.capabilities}"]
+  template_url = var.template_url
+  parameters   = var.parameters
+  capabilities = var.capabilities
 
-  on_failure         = "${var.on_failure}"
-  timeout_in_minutes = "${var.timeout_in_minutes}"
+  on_failure         = var.on_failure
+  timeout_in_minutes = var.timeout_in_minutes
 
-  policy_body  = "${var.policy_body}"
+  policy_body = var.policy_body
 }
