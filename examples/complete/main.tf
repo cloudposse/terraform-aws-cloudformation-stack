692397546becdfef170d4719312bbb12a076c170
milldr <miller0daniel@gmail.com>
2025-09-26 11:57:43 -0400

    Add StackSuffix parameter to CloudFormation stack module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,11 +2,19 @@ provider "aws" {
   region = var.region
 }
 
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
 module "cloudformation_stack" {
   source        = "../../"
   template_body = file("${path.module}/example.template")
-  parameters    = var.parameters
-  capabilities  = var.capabilities
+  parameters = merge(var.parameters, {
+    StackSuffix = "test-${formatdate("YYYYMMDD-hhmmss", timestamp())}-${random_string.suffix.result}"
+  })
+  capabilities = var.capabilities
 
   context = module.this.context
 }
